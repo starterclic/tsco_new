7909dab5326e31f1dfd4b5dd615e4b6d1b18eb5c
spencerthomas <tom@cognitives.io>
2020-07-30 12:05:24 +1000

    updated site title

--- packages/landing-gatsby/gatsby-config.js
+++ packages/landing-gatsby/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `A react next landing page`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Tom Spencer - Technology, Startups & Education`,
+    description: `I love ideas, tech and education.`,
+    author: `@spencer_thomas`,
   },
   plugins: [
     {
